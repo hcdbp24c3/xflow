811189bdf4d7aaedac2b74d9e0f07fc9e5b74e5f
6zev <h0dev@hotmail.com>
2026-09-13 06:36:13 +0000

    fix: disable R8 minification to fix build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -47,12 +47,6 @@ android {
             } else {
                 signingConfigs.getByName("debug")
             }
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
     }
 }
